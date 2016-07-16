f2890b35ab56780bd015cb0048f4df071eb2d264
Philip Neustrom <philipn@gmail.com>
2016-07-15 22:07:58 -0700

    Fix assertion parenthesis

--- container_transform/compose.py
+++ container_transform/compose.py
@@ -237,8 +237,8 @@ class ComposeTransformer(BaseTransformer):
             parts = vol.split(':')
             rwo_value = None
 
-            assert(len(parts) <= 3,
-                   "Volume string '{}' has too many colons".format(vol))
+            assert len(parts) <= 3, \
+                "Volume string '{}' has too many colons".format(vol)
 
             if len(parts) == 3:
                 # Is form 'service:name:ro' or 'container:name:ro'
